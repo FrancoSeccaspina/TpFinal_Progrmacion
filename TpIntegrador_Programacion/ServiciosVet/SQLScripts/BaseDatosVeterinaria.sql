29de53bcb5c870033e4069f767bcc7ea6dbfc772
gjimenez <gustavo.jimenez.crespo@gmail.com>
2024-06-20 16:28:07 -0300

    Agregar logica

---
+++ TpIntegrador_Programacion/ServiciosVet/SQLScripts/BaseDatosVeterinaria.sql
@@ -0,0 +1,37 @@
+IF DB_ID('BaseDatosVeterinaria') IS NULL
+BEGIN
+    CREATE DATABASE BaseDatosVeterinaria;
+END;
+
+USE BaseDatosVeterinaria;
+
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Usuarios')
+BEGIN
+    CREATE TABLE Usuarios (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    NickName VARCHAR(10) NOT NULL,
+    Contra INT NOT NULL
+);
+END;
+
+CREATE TABLE Clientes (
+    ID INT PRIMARY KEY IDENTITY(1,1),
+    DNI NVARCHAR(20) NOT NULL,
+    Nombre NVARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Especies (
+    ID INT PRIMARY KEY IDENTITY(1,1),
+    Nombre NVARCHAR(100) not null,
+    EdadMadurez INT not null,
+    PesoPromedio DECIMAL(10,2) not null
+);
+
+CREATE TABLE Animales (
+    ID INT PRIMARY KEY IDENTITY(1,1),
+    Nombre NVARCHAR(100) not null,
+    Peso DECIMAL(10,2) not null,
+    Edad INT not null, 
+    IDCliente INT FOREIGN KEY REFERENCES Clientes(ID),
+    IDEspecie INT FOREIGN KEY REFERENCES Especies(ID)
+);
