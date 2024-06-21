906dffeedcdd3d672d79f07e1fd265dd43f5bda2
Franco Seccaspina <106563115+FrancoSeccaspina@users.noreply.github.com>
2024-06-21 13:44:36 -0300

    Add files via upload

---
+++ TpIntegrador_Programacion/ServiciosVet/Models/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiciosVet.Models
+{
+    public class Animal
+    {
+        public int id { get; set; }
+        public string Nombre {  get; set; }
+        public decimal Peso {  get; set; }
+        public int Edad {  get; set; }
+
+        //public int IDEspecie {  get; set; } preguntar si se tiene que declarar
+        //public int IDCliente {  get; set; } o se encarga la base de datos
+        public Animal(string nombre, decimal peso, int edad)
+        {
+            this.Nombre = nombre;
+            this.Peso = peso;
+            this.Edad = edad;
+        }
+
+        public Animal()
+        {
+        }
+    }
+
+}
