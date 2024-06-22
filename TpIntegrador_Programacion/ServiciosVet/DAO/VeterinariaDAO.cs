173640fe6dcd8e66c684a09fdc0a1bfcfd287803
gjimenez <gustavo.jimenez.crespo@gmail.com>
2024-06-22 01:24:02 -0300

    Agregar StringConexion en app.config

--- TpIntegrador_Programacion/ServiciosVet/DAO/VeterinariaDAO.cs
+++ TpIntegrador_Programacion/ServiciosVet/DAO/VeterinariaDAO.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using ServiciosVet.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +14,80 @@ namespace ServiciosVet.DAO
 {
     public class VeterinariaDAO
     {
+        public VeterinariaDAO()
+        {
+
+        }
+        public IDbConnection ObtenerConexion()
+        {
+            try
+            {
+                string StringConexion = ConfigurationManager.ConnectionStrings["MiConexionBaseDeDatosVeterinaria"].ConnectionString;
+                SqlConnection connection = new SqlConnection(StringConexion);
+                connection.Open();
+                Console.WriteLine("Conexión exitosa.");
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                throw;
+            }
+        }
+
+        public List<Usuario> ObtenerTablasUsuario()
+        {
+            IDbConnection connection = this.ObtenerConexion();
+            IDbCommand command = connection.CreateCommand();
+
+            command.CommandText = " SELECT NickName, Contra FROM Usuario";
+
+            IDataReader lector = command.ExecuteReader();
+            List<Usuario> listadin = new List<Usuario>();
+
+            while (lector.Read())
+            {
+
+                Usuario UsuarioObt = new Usuario()
+                {
+                    NickName = lector.GetString(2),
+                    Contra = lector.GetInt32(3),
+                };
+                listadin.Add(UsuarioObt);
+            }
+            connection.Close();
+            return listadin;
+        }
+
+        //public void AgregarUsuario()
+        //{
+        //    string query = "INSERT INTO Usuarios (NickName, Contra)VALUES ('usuario1', 123456);"
+        //}
+
+        public List<Cliente> ObtenerTablasClientes()
+        {
+            IDbConnection connection = this.ObtenerConexion();
+            IDbCommand command = connection.CreateCommand();
+
+            command.CommandText = " SELECT DNI, Nombre FROM Clientes";
+
+            IDataReader lector = command.ExecuteReader();
+            List<Cliente> listadin = new List<Cliente>();
+
+            while (lector.Read())
+            {
+
+                Cliente clienteObt = new Cliente()
+                {
+                    DNI = lector.GetString(2), 
+                    Nombre = lector.GetString(3) 
+
+                };
+                listadin.Add(clienteObt);
+            }
+
+            connection.Close();
+            return listadin;
+        }
     }
 }
--- TpIntegrador_Programacion/TpIntegrador_Programacion/ConfiguracionesSql/SQLscripts/BaseDatosVeterinaria.sql
+++ TpIntegrador_Programacion/TpIntegrador_Programacion/ConfiguracionesSql/SQLscripts/BaseDatosVeterinaria.sql
@@ -6,26 +6,27 @@ END;
 USE BaseDatosVeterinaria;
 
 CREATE TABLE Usuarios (
-    Id INT PRIMARY KEY IDENTITY(1,1),
+    Id INT PRIMARY KEY IDENTITY,
     NickName VARCHAR(10) NOT NULL,
     Contra INT NOT NULL
 );
 
 CREATE TABLE Clientes (
-    ID INT PRIMARY KEY IDENTITY(1,1),
+    ID INT PRIMARY KEY IDENTITY,
     DNI NVARCHAR(20) NOT NULL,
     Nombre NVARCHAR(100) NOT NULL
+	--Columna posible Activa Descativado
 );
 
 CREATE TABLE Especies (
-    ID INT PRIMARY KEY IDENTITY(1,1),
+    ID INT PRIMARY KEY IDENTITY,
     Nombre NVARCHAR(100) not null,
     EdadMadurez INT not null,
     PesoPromedio DECIMAL(10,2) not null
 );
 
 CREATE TABLE Animales (
-    ID INT PRIMARY KEY IDENTITY(1,1),
+    ID INT PRIMARY KEY IDENTITY,
     Nombre NVARCHAR(100) not null,
     Peso DECIMAL(10,2) not null,
     Edad INT not null,
