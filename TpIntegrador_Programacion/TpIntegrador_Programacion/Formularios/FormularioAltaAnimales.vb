1e49db05d77c88c69cdff566e981414fcb1cc9b3
Franco Seccaspina <106563115+FrancoSeccaspina@users.noreply.github.com>
2024-06-21 23:45:30 -0300

    Update FormularioAltaAnimales.vb

--- TpIntegrador_Programacion/TpIntegrador_Programacion/Formularios/FormularioAltaAnimales.vb
+++ TpIntegrador_Programacion/TpIntegrador_Programacion/Formularios/FormularioAltaAnimales.vb
@@ -1,3 +1,25 @@
-﻿Public Class FormularioAltaAnimales
+Public Class FormularioAltaAnimales
+    Private Sub Btn_SeleccionarImagen_Click(sender As Object, e As EventArgs) Handles Btn_SeleccionarImagen.Click
+        Dim imagen As New OpenFileDialog()
+        imagen.Filter = "Archivos de Imagen|*.jpg;*.jpeg;*.png;*.bmp;*.gif"
 
+        If imagen.ShowDialog() = DialogResult.OK Then
+            PictureBox1.Image = Image.FromFile(imagen.FileName)
+            MessageBox.Show("¡Imagen adjuntada con exito!")
+        End If
+    End Sub
+
+    Private Sub Btn_AgregarAnimal_Click(sender As Object, e As EventArgs) Handles Btn_AgregarAnimal.Click
+        If TxtNombreAnimal.Text.Trim() <> "" And TxtEdadAnimal.Text.Trim() <> "" And TxtPesoAnimal.Text.Trim() <> "" Then
+            TxtNombreAnimal.Text = ""
+            TxtEdadAnimal.Text = ""
+            TxtPesoAnimal.Text = ""
+
+            MessageBox.Show("¡Animal Agregado con exito!")
+        Else
+            MessageBox.Show("Por favor, complete todos los campos y asajunte una foto")
+        End If
+
+
+    End Sub
 End Class
